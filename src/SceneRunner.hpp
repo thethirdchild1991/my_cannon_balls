4acdc51f5b2f3409b601c323c4a896f0a4f99e13
denis <denis.dylko@gmail.com>
2019-02-23 23:18:21 +0300

    .

--- src/SceneRunner.hpp
+++ src/SceneRunner.hpp
@@ -7,23 +7,58 @@
 #include<iostream>
 #endif
 
-
 #include <GLFW/glfw3.h>
+#include <GL/glew.h>
+// #include <glad/glad.h>
+
 
 
 class SceneRunner
 {
 private:
-    /* data */
+    GLFWwindow * window;
+    int fbw, fbh;
 public:
-    SceneRunner(/* args */){
+    SceneRunner(const std::string& winTitle, 
+    int width = WIN_WIDTH, int height = WIN_HEIGHT, 
+    int samples = 0, 
+    GLclampf rst_red = 0.0,  GLclampf rst_green = 0.0, GLclampf rst_blue = 0.0, GLclampf rst_alpha = 0.0 ){
         //do all init stuff
         if( !glfwInit() ){
 #ifdef DEBUG_IO
             std::cout << "GLFW init failed" << std::endl;
-            exit(-1);
+            exit( EXIT_FAILURE );
 #endif            
         }
+
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+        if(samples > 0) {
+            glfwWindowHint(GLFW_SAMPLES, samples);
+        }
+
+        window = glfwCreateWindow( WIN_WIDTH, WIN_HEIGHT, winTitle.c_str(), NULL, NULL );
+        if( ! window ) {
+            std::cout << "Failed to open GLFW window" << std::endl;
+            glfwTerminate();
+            exit( EXIT_FAILURE );
+        }
+        glfwMakeContextCurrent(window);
+
+        glfwGetFramebufferSize(window, &fbw, &fbh);
+
+        if (glewInit() != GLEW_OK) {
+            std::cout << "Failed to initialize GLEW" << std::endl;            
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+
+        glClearColor(rst_red, rst_green, rst_blue, rst_alpha);
+
+
     }
     ~SceneRunner(){
         //clean up
--- src/config.hpp
+++ src/config.hpp
@@ -2,6 +2,8 @@
 #define CONFIG_HPP
 
 #define DEBUG_IO
+#define WIN_WIDTH 640
+#define WIN_HEIGHT 480
 
 
 #endif
